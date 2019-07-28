0ef211fc6ad86b5669dc19ddc83f85966da05d8b
AlexanderLuther <helmuthalexander-luthermontejo@cunoc.edu.gt>
2019-07-27 21:48:03 -0600

    Ejecutable Agregado

--- src/Frontend/PanelInicio.java
+++ src/Frontend/PanelInicio.java
@@ -28,17 +28,21 @@ public class PanelInicio extends javax.swing.JPanel {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setLayout(new java.awt.GridBagLayout());
 
         jLabel1.setBackground(new java.awt.Color(255, 255, 255));
-        jLabel1.setIcon(new javax.swing.ImageIcon("/home/helmuthluther/Descargas/Bibioteca-800x400.jpg")); // NOI18N
         add(jLabel1, new java.awt.GridBagConstraints());
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Bibioteca-800x400.jpg"))); // NOI18N
+        add(jLabel2, new java.awt.GridBagConstraints());
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
